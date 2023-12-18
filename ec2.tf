3d47d800d9a9eea901115a4010c753154ea810cb
kizoka13 <kizoka13@gmail.com>
2023-12-18 11:59:38 -0500

    new change

--- ec2.tf
+++ ec2.tf
@@ -7,3 +7,12 @@ resource "aws_instance" "server1" {
  }
 }
 
+resource "aws_instance" "server2" {
+ instance_type = var.instance_type 
+ ami = data.aws_ami.ami1.id
+ tags = {
+   Name = "Terraform-sever2"
+   team = "dev"
+ }
+}
+
